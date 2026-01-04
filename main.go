9fcbbe76acba70263a9aeccd387d69ef3873b60c
lauchimoon <laurentino.luna06@gmail.com>
2026-01-04 15:03:37 -0300

    Add making function from signature

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "strings"
 )
 
 type FuncArg struct {
@@ -16,17 +17,11 @@ type Function struct {
 }
 
 func main() {
-    f := FunctionMake("int", "f", []FuncArg{
-        FuncArgMake("int", "a"),
-        FuncArgMake("int", "b"),
-    })
-    g := FunctionMake("bool", "g", []FuncArg{
-        FuncArgMake("int", "a"),
-        FuncArgMake("int", "b"),
-    })
+    f := FunctionMakeFromSignature("int(int, int)")
+    g := FunctionMakeFromSignature("bool(int, int)")
 
-    fmt.Println(f)
-    fmt.Println(g)
+    fmt.Println("f:", f)
+    fmt.Println("g:", g)
     fmt.Println("f matches g criteria?", f.MatchesCriteria(g))
 }
 
@@ -45,6 +40,31 @@ func FuncArgMake(typ, name string) FuncArg {
     }
 }
 
+func FunctionMakeFromSignature(sig string) Function {
+    openParenIndex := strings.IndexByte(sig, '(')
+    retType := sig[:openParenIndex]
+
+    return Function{
+        RetType: retType,
+        Name: "x",
+        Args: getArgs(sig),
+    }
+}
+
+func getArgs(sig string) []FuncArg {
+    openParenIndex := strings.IndexByte(sig, '(')
+    closeParenIndex := strings.IndexByte(sig, ')')
+    argsSig := sig[openParenIndex+1:closeParenIndex]
+
+    args := []FuncArg{}
+    for i, typ := range strings.Split(argsSig, ",") {
+        typ = strings.Trim(typ, " ")
+        args = append(args, FuncArgMake(typ, fmt.Sprintf("x%d", i)))
+    }
+
+    return args
+}
+
 func (f Function) String() string {
     s := f.RetType + " " + f.Name + "("
     for i, arg := range f.Args {
@@ -55,7 +75,7 @@ func (f Function) String() string {
         }
     }
 
-    return s + ")"
+    return s + ");"
 }
 
 // Names don't matter for now. We only care about:
